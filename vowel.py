30d60321ba6a2749f8628367532d8b2a9951a632
TNikitha4 <46804949+TNikitha4@users.noreply.github.com>
2019-01-31 19:55:02 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,9 @@
+# nikitha
+ch=input()
+if(ch=='a' or ch=='A' or ch=='e' or ch=='E' or ch=='i' or ch=='I' or ch=='o' or ch=='O' or ch=='u' or ch=='U'):
+    print("vowel")
+elseif((ch>='a' and ch<='z')or(ch<='A' and ch<='Z')):
+    print("consonant")
+else:
+    print("invalid")
+    
